3b12fd7f1b745138adbeb13b6c2b78f074093886
zxin <15670465+ichordev@users.noreply.github.com>
2023-06-15 19:41:47 +1000

    Unfinished changes

---
+++ source/bindbc/bgfx/config.d
@@ -0,0 +1,17 @@
+/+
++                Copyright 2023 Aya Partridge
++ Distributed under the Boost Software License, Version 1.0.
++     (See accompanying file LICENSE_1_0.txt or copy at
++           http://www.boost.org/LICENSE_1_0.txt)
++/
+module bindbc.bgfx.config;
+
+enum staticBinding = (){
+	version(BindBC_Static)        return true;
+	else version(BindBgfx_Static) return true;
+	else return false;
+}();
+
+import bindbc.common.codegen;
+
+mixin(makeFnBindFns(staticBinding));
--- source/bindbc/bgfx/package.d
+++ source/bindbc/bgfx/package.d
@@ -1,5 +1,10 @@
+/+
++                Copyright 2023 Aya Partridge
++ Distributed under the Boost Software License, Version 1.0.
++     (See accompanying file LICENSE_1_0.txt or copy at
++           http://www.boost.org/LICENSE_1_0.txt)
++/
 module bindbc.bgfx;
 
-public import bindbc.bgfx.types;
-public import bindbc.bgfx.funcs;
-public import bindbc.bgfx.dynload;
+public import bindbc.bgfx.config;
+public import bgfx;
